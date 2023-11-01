fd48ee842bc174cae20e4e24e3a8f17d5c8eebf3
Aziyck <75189288+Aziyck@users.noreply.github.com>
2023-11-01 19:00:05 +0200

    Add files via upload

--- smtQT/happyText.js
+++ smtQT/happyText.js
@@ -32,7 +32,7 @@ function initparticles() {
     $.each($(".particletext.confetti"), function(){
        var confetticount = ($(this).width()/50)*10;
        for(var i = 0; i <= confetticount; i++) {
-          $(this).append('<span class="particle c' + $.rnd(1,4) + '" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(6,12) + 'px; height:' + $.rnd(1,5) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+          $(this).append('<span class="particle c' + $.rnd(1,4) + '" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(10,15) + 'px; height:' + $.rnd(1,6) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
        }
     });
  }
@@ -41,7 +41,7 @@ function initparticles() {
    $.each($(".particletext.confettiTwo"), function(){
       var confetticount = ($(this).width()/50)*10;
       for(var i = 0; i <= confetticount; i++) {
-         $(this).append('<span class="particle c' + $.rnd(1,4) + '" style="top:' + $.rnd(20,30) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(6,12) + 'px; height:' + $.rnd(1,5) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+         $(this).append('<span class="particle c' + $.rnd(1,4) + '" style="top:' + $.rnd(20,30) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(6,12) + 'px; height:' + $.rnd(3,8) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
       }
    });
 }
