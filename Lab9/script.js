e0e0055be9469d633613f01a6a19949c8a65ad71
Aziyck <75189288+Aziyck@users.noreply.github.com>
2023-11-09 22:06:43 +0200

    Add files via upload

--- Lab9/script.js
+++ Lab9/script.js
@@ -1,11 +1,43 @@
+//max number of particles that can exist one on screen
+const NUMER_OF_ENTITIES = 1000;
+
+//the number of particles created on mouse mouvement
+const MIN_PARTICLES_CREATED_ON_MOUSE_MOVEMENT = 1;
+const MAX_PARTICLES_CREATED_ON_MOUSE_MOVEMENT = 3;
+
+//the spawn reagion aground the mouse
+const SPREAD_X = 10;
+const SPREAD_Y = 10;
+
+//size of circles
+const MIN_SIZE = 5;
+const MAX_SIZE = 100;
+
+//speed of spreading of cirles
+const SPEED_LEFT = 0.5;
+const SPEED_RIGHT = 0.5;
+const SPEED_UP = 0.5;
+const SPEED_DOWN = 0.5;
+
+// 256 >= n >= 0
+const INITIAL_RED_SHADE = 256;
+
+//the higher the faster
+const SPEED_OF_DECAY = 0.15;
+const MIN_SIZE_UNTIL_DECAYING = 1;
+
+//the lower the faster n >= 0
+const SPEED_OF_CHANGING_COLOR = 1;
+
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-
-const particle = [];
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -17,129 +49,88 @@ const mouse = {
     y: undefined,
 }
 
-canvas.addEventListener('click', function(mouseclick){
-    mouse.x = mouseclick.x;
-    mouse.y = mouseclick.y;
-    for (let j = 0; j<getRandomArbitrary(5,20); j++){
-        let d = new Drop();
-        d.x = getRandomArbitrary(10, canvas.width);
-        d.y = getRandomArbitrary(10, canvas.height);
-        particle.push(d);
-    }
-})
+const particle = [];
+
+// const MIN_PARTICLES_PER_CLICK = 1;
+// const MAX_PARTICLES_PER_CLICK = 10;
+// canvas.addEventListener('click', function(mouseclick){
+//     mouse.x = mouseclick.x;
+//     mouse.y = mouseclick.y;
+//     for (let j = 0; j<getRandom(MIN_PARTICLES_PER_CLICK, MAX_PARTICLES_PER_CLICK); j++){
+//         let d = new Shape();
+//         d.x = getRandom(MIN_SIZE, canvas.width);
+//         d.y = getRandom(MIN_SIZE, canvas.height);
+//         particle.push(d);
+//     }
+// })
 
 canvas.addEventListener('mousemove', function(mouseclick){
     mouse.x = mouseclick.x;
     mouse.y = mouseclick.y;
-    for (let j = 0; j<getRandomArbitrary(1,3); j++){
-        particle.push(new Drop);
-        // if(getRandomArbitrary(1,20) > 10){
-        //     // particle.push(new Bubble);
-        // }else{
-        //     particle.push(new Drop);
-        // }
-            
+    for (let j = 0; j<getRandom(MIN_PARTICLES_CREATED_ON_MOUSE_MOVEMENT, MAX_PARTICLES_CREATED_ON_MOUSE_MOVEMENT); j++){
+        if (particle.length >= NUMER_OF_ENTITIES) {}
+        else particle.push(new Shape);
     }
 })
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  }
 
-class Bubble{
+class Shape{
     constructor(){
-        this.x=mouse.x;
-        this.y=mouse.y;
-        this.size=getRandomArbitrary(3,12);
-        this.speedX=Math.random()*2.5;
-        this.speedY=Math.random()*10;
+        this.x=mouse.x + getRandom(-SPREAD_X, SPREAD_X);
+        this.y=mouse.y + getRandom(-SPREAD_Y, SPREAD_Y);
+
+        this.size=getRandom(MIN_SIZE, MAX_SIZE);
+
+        this.speedX=getRandom(-SPEED_LEFT, SPEED_RIGHT);
+        this.speedY=getRandom(-SPEED_UP, SPEED_DOWN);
+
+        this.red = INITIAL_RED_SHADE;
+        this.green = 0;
+        this.blue = 0;
+        this.color = 'rgb('+ this.red + ',' + this.green + ','+ this.blue +')';
+        this.colorChangeCounter = 0;
     }
+
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
-        if(this.size>0.2) this.size -= 0.1;
-    }
-    draw(){
-        ctx.beginPath();
-        ctx.strokeStyle='pink';
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
-        ctx.stroke();
-        ctx.closePath();
-    }
-}
 
-// let col = window.prompt("Color: ");
-let col = null;
+        if(this.size > MIN_SIZE_UNTIL_DECAYING) this.size -= SPEED_OF_DECAY;
 
-let c = true
-function couterRChange(){
-    if(c == true)
-        counterR++;
-    else
-        counterR--;
-}
-
-counterR = 100;
-counterG = 1;
-counterB = 1;
-couter = 1;
-class Drop{
-    constructor(){
-        this.x=mouse.x + getRandomArbitrary(-100,100);
-        this.y=mouse.y + getRandomArbitrary(-100,100);
-        this.size=getRandomArbitrary(5,80);
-        this.speedX=getRandomArbitrary(-0.2, 0.2);
-        this.speedY=getRandomArbitrary(-0.3, 0.3);
-        // this.counter = 1000;
-        if(col == null || col == ""){
-            this.color = 'rgb('+ counterR + ',' + counterG + ','+ counterB+')';
-        }else{
-            this.color = col;
-        }
-    }
-    update(){
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if(this.size>0.2) this.size -= 0.2;
+        this.red = this.colourModify(this.red);
+        this.color = 'rgb('+ this.red + ',' + this.green + ','+ this.blue +')';
     }
     draw(){
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
-        // ctx.arc(this.x, this.y, this.size, Math.PI, Math.PI*2, true);
-        // ctx.lineTo(this.x, this.y-this.size*2);
-        // ctx.lineTo(this.x-this.size, this.y);
-        // ctx.stroke();
         ctx.fillStyle = this.color;
-        if (col == null || col == ""){
-            if(couter >= 400){
-                couter = 1;
-                couterRChange();
-            }
-            couter++; 
-            if(counterR >= 200 || counterR <= 50) {c = !c};
-        }
         ctx.fill();
         ctx.closePath();
     }
-}
 
-function init(){
-    for (let i = 0; i<100; i++){
-        particle.push(new Bubble());
+    colourModify(colorParameter){
+        if(this.colorChangeCounter >= SPEED_OF_CHANGING_COLOR){
+            
+            this.colorChangeCounter = 0;
+            colorParameter--;
+        }
+        this.colorChangeCounter++;
+
+        return colorParameter;
     }
 }
-init();
 
 function drawParticle(){
     for (let i = 0; i < particle.length; i++){
         particle[i].update();
         particle[i].draw();
-        if(particle[i].size<=0.2){
+        if(particle[i].size<= MIN_SIZE_UNTIL_DECAYING){
             particle.splice(i,1);
             i--;
         }
     }
 }
+
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawParticle();
